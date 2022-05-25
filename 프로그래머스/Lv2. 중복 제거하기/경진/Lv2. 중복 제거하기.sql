474d4067eece1095c538389a23dd22b160ea7a48
WooKyungjin <wkj1103@naver.com>
2022-05-25 11:44:35 +0900

    프로그래머스/level2/중복 제거하기

--- 프로그래머스/Lv2. 중복 제거하기/경진/Lv2. 중복 제거하기.sql
+++ 프로그래머스/Lv2. 중복 제거하기/경진/Lv2. 중복 제거하기.sql
@@ -1,4 +1,3 @@
 -- https://programmers.co.kr/learn/courses/30/lessons/59408
-select count(*)
-from (select distinct NAME from ANIMAL_INS) as TB_NAME
-where TB_NAME.NAME is not null
+select count(distinct NAME)
+from ANIMAL_INS
