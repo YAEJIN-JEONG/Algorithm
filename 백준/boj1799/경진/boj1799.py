2ca3ade8182f14b7897f57d84774fca35a63f965
kyungjin <wkj1103@naver.com>
2022-06-13 23:19:17 +0900

    비숍: 분할집합, 백트래킹 (refactoring)

--- 백준/boj1799/경진/boj1799.py
+++ 백준/boj1799/경진/boj1799.py
@@ -10,15 +10,19 @@ candidates = [[] for _ in range(2)]
 for i in range(n):
     for j in range(n):
         if board[i][j] == '1':
-            candidates[(i + j) % 2].append((i, j))
+            candidates[(i + j) % 2].append([i, j])
 
 # 흑, 백 칸에 각각 놓는 비숍 개수
 answer = [0, 0]
 
+# 대각선 방향 확인
+slash = [False] * (2 * n + 1)
+backslash = [False] * (2 * n + 1)
+
 
 # 백트래킹
 def backtrack(start, depth, selected, k):
-    global n, candidates, answer
+    global n, candidates, answer, slash, backslash
 
     # 최대값 갱신이 불가능한 경우
     if answer[k] == len(candidates[k]) or depth + (len(candidates[k]) - start) <= answer[k]:
@@ -28,21 +32,14 @@ def backtrack(start, depth, selected, k):
 
     # 가능한 좌표에 대해 dfs
     for idx in range(start, len(candidates[k])):
-        x, y = candidates[k][idx]
+        x, y = candidates[k][idx][0], candidates[k][idx][1]
 
-        if is_valid(x, y, selected):
+        if not slash[x + y] and not backslash[x - y + n]:
             selected.add((x, y))
+            slash[x + y] = backslash[x - y + n] = True
             backtrack(idx + 1, depth + 1, selected, k)
+            slash[x + y] = backslash[x - y + n] = False
             selected.remove((x, y))
-
-
-# 비숍을 놓을 수 있는 위치인지 확인
-def is_valid(x, y, selected):
-    for x2, y2 in selected:
-        if x2 - y2 == x - y or x + y == x2 + y2:
-            return False
-
-    return True
 
 
 # 흑, 백 각각 백트래킹
