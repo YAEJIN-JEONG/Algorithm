655a22214682b3929966fc0bec2f8f50951d49cb
chanjin5212 <chanjin5212@gmail.com>
2022-06-27 13:57:51 +0900

    백준/9935/문자열 폭발

---
+++ 백준/boj9935/찬진/Q9935.java
@@ -0,0 +1,41 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+//https://www.acmicpc.net/problem/9935
+public class Q9935 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
+        String input = reader.readLine();
+        Stack<Character> stack = new Stack<>();
+        for (int i=0; i<str.length(); i++) {
+            stack.push(str.charAt(i));
+
+            if (stack.size() >= input.length()) {
+                boolean flag = true;
+                for (int j=0; j<input.length(); j++) {
+                    if (stack.get(stack.size()-input.length()+j) != input.charAt(j)) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    for (int j=0; j<input.length(); j++) {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        int size = stack.size();
+        for (int i=0; i<size; i++) {
+            sb.append(stack.pop());
+        }
+        System.out.println(sb.length() == 0 ? "FRULA" : sb);
+
+    }
+}
